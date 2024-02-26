7dd0c5a0beb71d47ae67756b892e05aabb08574f
Yassine Azma <96583432+YassineAzma@users.noreply.github.com>
2024-02-26 09:21:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import time
 
-from bloch import arb_rot, free_precession, y_rot
+import bloch
 import numpy as np
+import sequence
+from visualise import non_selective_animation
 
 
 def main():
-    initial_magnetisation = np.array([0, 0, 1])
-    t1 = 600
-    t2 = 100
-    tr = 500
-    flip = np.pi / 3
+    isochromats = np.linspace(-1000, 1000, 500)
+    positions = np.zeros((len(isochromats), 3))
+    pulse_1 = sequence.rf.hypsec_pulse(5e-3, 2400, 5, 1e-6)
+    pulse_1.get_info()
+    pulse_1.display()
+    pulse_1.set_delta_frequency(2.89 * 42.58 * -2.3)
+    pulse_amplitude = pulse_1.get_optimal_amplitude(30e-6, (-2000.0, 2000.0),
+                                                    np.pi, display=True)
+    pulse_1.set_amplitude(pulse_amplitude)
 
-    magnetisation = y_rot(flip) @ initial_magnetisation
-    a, b = free_precession(1, t1, t2, 0)
-    magnetisation = a @ magnetisation + b
-    print(magnetisation)
+    delta_time = 1e-5
+
+    init_time = time.perf_counter()
+    magnetisation = bloch.simulate.non_selective_rot3d_matrix(t1=np.inf, t2=23.34, df=isochromats,
+                                                              rf_pulse=pulse_1.get_waveform(delta_time),
+                                                              delta_time=delta_time)
+    end_time = time.perf_counter()
+    sim_length, num_iso, _ = magnetisation.shape
+    print(f"Time taken = {round(end_time - init_time, 2)}s to "
+          f"simulate {sim_length} time steps for {num_iso} isochromats!")
+
+    animation = non_selective_animation(test_pulse, magnetisation, isochromats, delta_time, play=True,
+                                        repeat=False, phase_mode=0, save_path=None)
 
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
