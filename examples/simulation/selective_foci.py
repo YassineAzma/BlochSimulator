7302bbb2830b1ae56967b63aa980c9ddb548fd20
Yassine Azma <96583432+YassineAzma@users.noreply.github.com>
2024-03-02 08:27:51 +0000

    Formatting

--- examples/simulation/selective_foci.py
+++ examples/simulation/selective_foci.py
@@ -3,20 +3,23 @@ import bloch
 import numpy as np
 import sequence
 import visualise
+from sequence.gradient import calculate_excitation_amplitude
 
 num_isochromats = 500
 positions = np.zeros((num_isochromats, 3))
 positions[:, 2] = np.linspace(-15e-3, 15e-3, num_isochromats)
 
-gradient_x = sequence.gradient.rect_gradient(7.68e-3, 0, 1e-6)
-gradient_y = sequence.gradient.rect_gradient(7.68e-3, 0, 1e-6)
-foci_pulse, gradient_z = sequence.rf.foci_pulse(7.68e-3, 1500, 3.9,
-                                                2.5e-3, 25e-3, 1e-6)
+gradient_x = sequence.gradient.rect_gradient(10.24e-3, 0, 1e-6)
+gradient_y = sequence.gradient.rect_gradient(10.24e-3, 0, 1e-6)
+grad_strength = calculate_excitation_amplitude(2000, 5e-3)
+foci_pulse, gradient_z = sequence.rf.foci_pulse(10.24e-3, 2000, 2.5,
+                                                grad_strength, 25e-3, 1e-6)
 foci_pulse.get_info()
 foci_pulse.display()
 pulse_amplitude = foci_pulse.get_optimal_amplitude(30e-6, (-2000.0, 2000.0),
                                                    np.pi, delta_time=5e-5, display=True)
 foci_pulse.set_amplitude(pulse_amplitude)
+foci_pulse.set_delta_frequency(42.58 * 2.89 * 0)
 
 delta_time = 1e-5
 
