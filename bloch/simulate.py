56746804aed81274f1cb6b9c18abc05988ffb5a6
Yassine Azma <96583432+YassineAzma@users.noreply.github.com>
2024-03-11 08:17:56 +0000

    Update simulate.py
    Add support for spectral-spatial simulations.

--- bloch/simulate.py
+++ bloch/simulate.py
@@ -50,10 +50,9 @@ def non_selective_rot3d_matrix(t1: float, t2: float, df: np.ndarray,
             magnetisation[step, iso_num, :] = a @ magnetisation[step - 1, iso_num, :] + b
 
             # RF Rotation
-            if rf_pulse is not None:
-                current_rot = mag_rf[step - 1], phase_rf[step - 1]
-                rf_rotation = arb_rot(*current_rot)
-                magnetisation[step, iso_num, :] = rf_rotation @ magnetisation[step, iso_num, :]
+            current_rot = mag_rf[step - 1], phase_rf[step - 1]
+            rf_rotation = arb_rot(*current_rot)
+            magnetisation[step, iso_num, :] = rf_rotation @ magnetisation[step, iso_num, :]
 
     return magnetisation
 
@@ -61,10 +60,10 @@ def non_selective_rot3d_matrix(t1: float, t2: float, df: np.ndarray,
 @numba.njit((float64[:, :, ::1])(float64, float64, float64[:, ::1], complex128[::1],
                                  float64[:], float64[:], float64[:], float64),
             parallel=True, cache=True)
-def selective_rot3d_matrix(t1: float, t2: float, position: np.ndarray,
-                           rf_pulse: np.ndarray,
-                           grad_x: np.ndarray, grad_y: np.ndarray, grad_z: np.ndarray,
-                           delta_time: float = 1e-6):
+def spatial_selective_rot3d_matrix(t1: float, t2: float, position: np.ndarray,
+                                   rf_pulse: np.ndarray,
+                                   grad_x: np.ndarray, grad_y: np.ndarray, grad_z: np.ndarray,
+                                   delta_time: float = 1e-6):
     rf_waveform = rf_pulse * GAMMA_RAD * delta_time
     mag_rf = np.abs(rf_waveform)
     phase_rf = np.angle(rf_waveform)
@@ -85,7 +84,44 @@ def selective_rot3d_matrix(t1: float, t2: float, position: np.ndarray,
 
             # Gradient Rotation
             for grad_index, axis_position in enumerate(current_position):
-                gradient_rotation = z_rot(axis_position * gradient_waveform[grad_index, step - 1])
+                gradient_rotation = z_rot(axis_position * gradient_waveform[grad_index, step - 1].item())
+                magnetisation[step, iso_num, :] = gradient_rotation @ magnetisation[step, iso_num, :]
+
+            # RF Rotation
+            current_rot = mag_rf[step - 1], phase_rf[step - 1]
+            rf_rotation = arb_rot(*current_rot)
+            magnetisation[step, iso_num, :] = rf_rotation @ magnetisation[step, iso_num, :]
+
+    return magnetisation
+
+
+@numba.njit((float64[:, :, ::1])(float64, float64, float64[:], float64[:, ::1], complex128[::1],
+                                 float64[:], float64[:], float64[:], float64),
+            parallel=True, cache=True)
+def spectral_spatial_selective_rot3d_matrix(t1: float, t2: float, df: np.ndarray,
+                                            position: np.ndarray, rf_pulse: np.ndarray,
+                                            grad_x: np.ndarray, grad_y: np.ndarray, grad_z: np.ndarray,
+                                            delta_time: float = 1e-6):
+    rf_waveform = rf_pulse * GAMMA_RAD * delta_time
+    mag_rf = np.abs(rf_waveform)
+    phase_rf = np.angle(rf_waveform)
+
+    gradients = np.vstack((grad_x, grad_y, grad_z))
+    gradient_waveform = gradients * GAMMA_RAD * delta_time
+    sim_length = len(rf_pulse)
+
+    magnetisation = np.empty((sim_length, len(df), 3), dtype=np.float64)
+    magnetisation[0, :, :] = np.array([0.0, 0.0, 1.0])
+    for iso_num in numba.prange(len(df)):
+        a, b = free_precession(delta_time, t1, t2, df[iso_num])
+        current_position = position[iso_num, :]
+        for step in range(1, sim_length):
+            # Free precession
+            magnetisation[step, iso_num, :] = a @ magnetisation[step - 1, iso_num, :] + b
+
+            # Gradient Rotation
+            for grad_index, axis_position in enumerate(current_position):
+                gradient_rotation = z_rot(axis_position * gradient_waveform[grad_index, step - 1].item())
                 magnetisation[step, iso_num, :] = gradient_rotation @ magnetisation[step, iso_num, :]
 
             # RF Rotation
