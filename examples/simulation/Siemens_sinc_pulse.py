070ef2754eaa726126f7b31db115d44893551235
Yassine Azma <96583432+YassineAzma@users.noreply.github.com>
2024-03-11 08:17:10 +0000

    Update Siemens_sinc_pulse.py
    Minor tidy-up.

--- examples/simulation/Siemens_sinc_pulse.py
+++ examples/simulation/Siemens_sinc_pulse.py
@@ -1,5 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
 
 import bloch
 import sequence
@@ -9,27 +8,27 @@ from vendor.siemens import extract_rf_pulse
 # NON-SELECTIVE EXCITATION EXAMPLE
 
 # Import RF waveform from text file exported from IDEA simulator
-rf_pulse = extract_rf_pulse('sinc_pulse.txt', 0)
+sinc_waveform = extract_rf_pulse('sinc_pulse.txt', 0)
 
 # Convert the numpy array into an RF pulse object for pulse amplitude optimisation to achieve a 90 degree excitation.
 # Siemens uses a convention of 1us for each time interval, so we use a delta time of 1e-6.
 
-pulse = sequence.rf.create(rf_pulse, 1e-6)
-pulse_amplitude = pulse.get_optimal_amplitude(30e-6, (-1000.0, 1000.0), np.pi / 2, display=True)
-pulse.get_info()
-pulse.set_amplitude(pulse_amplitude)
+sinc_pulse = sequence.rf.create(sinc_waveform, 1e-6)
+pulse_amplitude = sinc_pulse.get_optimal_amplitude(30e-6, (-1000.0, 1000.0), np.pi / 2, display=True)
+sinc_pulse.get_info()
+sinc_pulse.set_amplitude(pulse_amplitude)
 
 # Prepare Bloch simulation
 # Rather than sampling the RF waveform every second, we can speed up the simulation by sampling every 10us without
 # changing the resulting frequency profile appreciably.
 
 delta_time = 1e-5
-rf_waveform = pulse.get_waveform(delta_time)
+rf_waveform = sinc_pulse.get_waveform(delta_time)
 off_resonances = np.linspace(-3000, 3000, 500)
 
 # The magnetisation returned has dimensions (number of time steps, number of off resonances, 3)
 magnetisation = bloch.simulate.non_selective_rot3d_matrix(t1=np.inf, t2=np.inf, df=off_resonances,
-                                                          rf_pulse=pulse.get_waveform(1e-5), delta_time=1e-5)
+                                                          rf_pulse=sinc_pulse.get_waveform(1e-5), delta_time=1e-5)
 
-visualise.non_selective_animation(pulse, magnetisation, off_resonances, 1e-5,
-                                  play=True, repeat=False, phase_mode=0, save_path=None)
+visualise.animate(magnetisation, delta_time, 'non_selective', off_resonances=off_resonances,
+                  rf_pulse=sinc_pulse, repeat=True)
