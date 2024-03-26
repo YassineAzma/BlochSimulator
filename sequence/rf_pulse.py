ec89802ba1f97277c7c36486de684547fd30ec74
Yassine Azma <96583432+YassineAzma@users.noreply.github.com>
2024-03-26 12:27:47 +0000

    Update rf_pulse.py
    Add BIR-4 pulse and some minor refactoring

--- sequence/rf_pulse.py
+++ sequence/rf_pulse.py
@@ -2,11 +2,12 @@ from __future__ import annotations
 
 import math
 import pprint
+from typing import Optional
 
 import numpy as np
 from matplotlib import pyplot as plt
 
-from bloch.simulate import non_selective_rot3d_matrix
+from bloch.simulate import run
 from constants import GAMMA_RAD
 from sequence.gradient import Gradient
 from sequence.object import SequenceObject, generate_times
@@ -51,6 +52,12 @@ class RFPulse(SequenceObject):
         comb_waveform = self.normalize(comb_waveform)
 
         return RFPulse(self.delta_time, self.times, comb_waveform)
+
+    def __copy__(self):
+        cls = self.__class__
+        result = cls.__new__(cls)
+        result.__dict__.update(self.__dict__)
+        return result
 
     def append(self, other_pulse: RFPulse | list[RFPulse], delay: float = 0.0) -> None:
         if not isinstance(other_pulse, list):
@@ -120,7 +127,7 @@ class RFPulse(SequenceObject):
             plt.subplots(2, 1)
             plt.subplot(2, 1, 1)
 
-        plt.plot(1e3 * self.times, magnitude)
+        plt.plot(1e3 * self.times, magnitude / np.max(magnitude))
         plt.xlabel('Time (ms)') if phase is None else None
         plt.ylabel('Normalised Amplitude')
         plt.grid()
@@ -142,23 +149,25 @@ class RFPulse(SequenceObject):
         return self.amplitude * flip_angle / self.get_flip_angle()
 
     def get_optimal_amplitude(self, max_b1: float, desired_range: tuple[float, float],
-                              flip_angle: float, delta_time: float = 1e-4, display: bool = False) -> float:
+                              flip_angle: float, delta_time: float = 1e-4, min_b1: float = 1e-6, display: bool = False) -> float:
 
-        num_amplitudes = int(max_b1 / 1e-6)
-        amplitudes = np.linspace(1e-6, max_b1, num_amplitudes)
+        num_amplitudes = int((max_b1 - min_b1) / 1e-6)
+        amplitudes = np.linspace(min_b1, max_b1, num_amplitudes + 1)
 
         initial_pulse = self.get_waveform(delta_time) / self.amplitude
         mult_num_iso = math.ceil(np.max(np.abs([value for value in desired_range])) / 500.0)
         frequency_limit = mult_num_iso * 500
 
-        off_resonance = np.linspace(-frequency_limit, frequency_limit, 100 + 10 * mult_num_iso)
+        off_resonance = np.linspace(-frequency_limit, frequency_limit, 50 + 10 * mult_num_iso)
         mxy_profiles = np.zeros((len(amplitudes), len(off_resonance)), dtype=np.float64)
         mz_profiles = np.zeros((len(amplitudes), len(off_resonance)), dtype=np.float64)
 
         for index, amplitude in enumerate(amplitudes):
             test_pulse = amplitude * initial_pulse
-            magnetisation = non_selective_rot3d_matrix(t1=np.inf, t2=np.inf, df=off_resonance,
-                                                       rf_pulse=test_pulse, delta_time=delta_time)
+            magnetisation = run(t1=np.inf, t2=np.inf, simulation_style='non_selective',
+                                df=off_resonance,
+                                rf_pulse=test_pulse, delta_time=delta_time,
+                                verbose=False)
             mxy_profiles[index, :] = np.abs(magnetisation[-1, :, 0] + 1j * magnetisation[-1, :, 1])
             mz_profiles[index, :] = magnetisation[-1, :, 2]
 
@@ -180,7 +189,7 @@ class RFPulse(SequenceObject):
 
         if display:
             fig, _ = plt.subplots(1, 2, figsize=(6, 6), sharex=True, sharey=True)
-            fig.text(0.5, 0.04, 'Off-Resonance (Hz)', ha='center')
+            fig.text(0.5, 0.04, 'Isochromat Frequency (Hz)', ha='center')
 
             plt.subplot(1, 2, 1)
             plt.imshow(mxy_profiles, cmap='jet', origin='lower', vmin=-1, vmax=1,
@@ -247,7 +256,7 @@ def binomial_pulse(subpulse_duration: float, interpulse_delay: float, binomial_o
 
 
 # Shaped Pulses
-def sinc_pulse(duration: float, bandwidth: float, delta_time: float,
+def sinc_pulse(duration: float, bandwidth: float, delta_time: float = 1e-6,
                hamming: bool = False) -> RFPulse:
     times = generate_times(delta_time, duration)
 
@@ -260,7 +269,7 @@ def sinc_pulse(duration: float, bandwidth: float, delta_time: float,
                          f"BW = {bandwidth}Hz)")
 
 
-def gaussian_pulse(duration: float, bandwidth: float, delta_time: float,
+def gaussian_pulse(duration: float, bandwidth: float, delta_time: float = 1e-6,
                    hamming: bool = True) -> RFPulse:
     times = generate_times(delta_time, duration)
 
@@ -273,7 +282,8 @@ def gaussian_pulse(duration: float, bandwidth: float, delta_time: float,
                          f"BW = {bandwidth}Hz)")
 
 
-def hermite_pulse(duration: float, bandwidth: float, order: int, factors: list[float], delta_time: float) -> RFPulse:
+def hermite_pulse(duration: float, bandwidth: float, order: int, factors: list[float],
+                  delta_time: float = 1e-6) -> RFPulse:
     if order < 2 and order % 2 != 0:
         raise ValueError('Order must be at least 2 and even!')
 
@@ -285,8 +295,8 @@ def hermite_pulse(duration: float, bandwidth: float, order: int, factors: list[f
 
     hermite_envelope = np.clip(hermite_envelope, -1, 1)
 
-    plt.plot(hermite_envelope)
-    plt.show()
+    # plt.plot(hermite_envelope)
+    # plt.show()
 
     gaussian_envelope = np.exp(-(np.pi * bandwidth * (times - duration / 2)) ** 2 / (4 * np.log(2)))
     magnitude = hermite_envelope * gaussian_envelope
@@ -300,11 +310,23 @@ def hermite_pulse(duration: float, bandwidth: float, order: int, factors: list[f
 
 
 # Adiabatic Pulses
-def hypsec_pulse(duration: float, bandwidth: int,
-                 empirical_factor: float, delta_time: float) -> RFPulse:
+def hypsec_pulse(duration: float, bandwidth: Optional[int] = None,
+                 empirical_factor: Optional[float] = None, beta: Optional[float] = None,
+                 delta_time: float = 1e-6) -> RFPulse:
+    num_viable_parameters = len([param for param in [bandwidth, empirical_factor, beta] if param is not None])
+
+    if num_viable_parameters != 2:
+        raise AttributeError(f'{num_viable_parameters} parameters provided! '
+                             f'Please provide two of the three parameters: empirical_factor, beta, and bandwidth!')
+
     times = generate_times(delta_time, duration)
 
-    beta = np.pi * bandwidth / empirical_factor
+    if beta is None:
+        beta = np.pi * bandwidth / empirical_factor
+    elif empirical_factor is None:
+        empirical_factor = np.pi * bandwidth / beta
+    else:
+        bandwidth = empirical_factor * beta / np.pi
 
     magnitude = 1 / np.cosh(beta * (times - duration / 2))
     frequency_sweep = -empirical_factor * beta * np.tanh(beta * (times - duration / 2))
@@ -316,13 +338,61 @@ def hypsec_pulse(duration: float, bandwidth: int,
                    duration=duration, bandwidth=bandwidth,
                    adiabatic_threshold=adiabatic_threshold,
                    title=f"HS Pulse ($T_p$ = {round(duration * 1e3, 2)}ms, "
-                         f"BW = {bandwidth}Hz, μ = {empirical_factor}, "
+                         f"BW = {bandwidth}Hz, μ = {round(empirical_factor, 2)}, "
                          f"β = {round(beta, 1)}s⁻¹)")
 
 
-def foci_pulse(duration: float, bandwidth: int, empirical_factor: float,
-               gradient_strength: float, max_gradient_strength: float,
-               delta_time: float) -> tuple[RFPulse, Gradient]:
+def bir4_pulse(duration: float, bandwidth: float, beta: float, flip_angle: float, delta_time: float = 1e-6) -> RFPulse:
+    times = generate_times(delta_time, duration)
+
+    def b1(times: np.ndarray) -> np.ndarray:
+        return 1 / np.cosh(beta * 4 * times / duration)
+
+    def phase(times: np.ndarray) -> np.ndarray:
+        return np.pi * bandwidth * duration / (2 * beta) * np.log(np.cosh(beta * 4 * times / duration))
+
+    first_segment = (times <= duration / 4)
+    first_magnitude_segment = np.where(first_segment,  b1(times), 0)
+    first_phase_segment = np.where(first_segment, phase(times), 0)
+
+    second_segment = (times >= duration / 4) & (times <= duration / 2)
+    second_magnitude_segment = np.where(second_segment, b1(duration / 2 - times), first_magnitude_segment)
+    second_phase_segment = np.where(second_segment, phase(duration / 2 - times) + np.pi + flip_angle / 2, first_phase_segment)
+
+    third_segment = (times >= duration / 2) & (times <= 3 * duration / 4)
+    third_magnitude_segment = np.where(third_segment, b1(times - duration / 2), second_magnitude_segment)
+    third_phase_segment = np.where(third_segment, phase(times - duration / 2) + np.pi + flip_angle / 2, second_phase_segment)
+
+    fourth_segment = (times >= 3 * duration / 4)
+    fourth_magnitude_segment = np.where(fourth_segment, b1(duration - times), third_magnitude_segment)
+    fourth_phase_segment = np.where(fourth_segment, phase(duration - times), third_phase_segment)
+
+    magnitude = fourth_magnitude_segment
+    phase = fourth_phase_segment
+
+    return RFPulse(delta_time, times, magnitude * np.exp(1j * phase),
+                   duration=duration, bandwidth=bandwidth,
+                   title=f"BIR4 Pulse ($T_p$ = {round(duration * 1e3, 2)}ms, "
+                         f"BW = {bandwidth}Hz, β = {round(beta, 1)}, "
+                         f"θ = {round(flip_angle * 180 / np.pi, 1)}°)")
+
+
+def foci_pulse(duration: float, gradient_strength: float, max_gradient_strength: float, bandwidth: Optional[int] = None,
+               empirical_factor: Optional[float] = None, beta: Optional[float] = None,
+               delta_time: float = 1e-6) -> tuple[RFPulse, Gradient]:
+    num_viable_parameters = len([param for param in [bandwidth, empirical_factor, beta] if param is not None])
+
+    if num_viable_parameters != 2:
+        raise AttributeError(f'{num_viable_parameters} parameters provided! '
+                             f'Please provide two of the three parameters: empirical_factor, beta, and bandwidth!')
+
+    if beta is None:
+        beta = np.pi * bandwidth / empirical_factor
+    elif empirical_factor is None:
+        empirical_factor = np.pi * bandwidth / beta
+    else:
+        bandwidth = empirical_factor * beta / np.pi
+
     times = generate_times(delta_time, duration)
 
     beta = np.pi * bandwidth / empirical_factor
@@ -355,23 +425,3 @@ def create(data: np.ndarray, delta_time: float) -> RFPulse:
                        duration=duration)
 
     return rf_pulse
-
-
-def get_full_width_arbitrary(off_resonance: np.ndarray, magnetisation: np.ndarray,
-                             threshold: float = 0.5, profile: str = 'excitation') -> float:
-    if profile == 'excitation':
-        excitation_profile = np.abs(magnetisation[-1, :, 0] + 1j * magnetisation[-1, :, 1])
-    elif profile == 'inversion':
-        excitation_profile = -magnetisation[-1, :, 2]
-    else:
-        raise ValueError("Profile must be 'excitation' or 'inversion'")
-
-    indices = np.where(excitation_profile > threshold)[0]
-    if len(indices) == 0:
-        return 0
-    else:
-        return off_resonance[indices[-1]].item() - off_resonance[indices[0]].item()
-
-
-def get_full_width_half_maximum(off_resonance: np.ndarray, magnetisation: np.ndarray) -> float:
-    return get_full_width_arbitrary(off_resonance, magnetisation, 0.5)
